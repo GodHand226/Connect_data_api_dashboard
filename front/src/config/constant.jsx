da06fb0bfa2341304260bd9e881ff819042e0a06
GodHand <dennis.chancy226@gmail.com>
2023-10-16 14:33:22 -0700

    Update frontend

--- front/src/config/constant.jsx
+++
@@ -1,9 +0,0 @@
-let BACKEND_SERVER = null;
-if (import.meta.env.REACT_APP_BACKEND_SERVER) {
-  BACKEND_SERVER = import.meta.env.REACT_APP_BACKEND_SERVER;
-} else {
-  BACKEND_SERVER = "http://localhost:5000/api";
-  // BACKEND_SERVER = "https://demandexcellence.net/api";
-}
-export const API_SERVER = BACKEND_SERVER;
-export const API_ENDPOINT = "http://api.dataapiconnect.com/api";
--- front/src/widgets/message/index.js
+++
@@ -1,4 +0,0 @@
-export * from "@/widgets/message/pay";
-export * from "@/widgets/message/standard";
-export * from "@/widgets/message/professional";
-export * from "@/widgets/message/enterprise";
